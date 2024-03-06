dcf5dac689a5a116d7b0fadfbc6c430fea4df53a
EdhsonOb <edhson.obed85@gmail.com>
2024-03-06 10:23:17 -0600

    sexto commit

--- app.js
+++ app.js
@@ -63,20 +63,20 @@ const processedRecords = records.map(record => {
     let clientName = '';
   
     // Procesar move_id
-    if (record.move_id && Array.isArray(record.move_id) && record.move_id.length > 1) {
+    if (Array.isArray(record.move_id) && record.move_id.length > 1) {
       secondMoveId = record.move_id[1].trim();
       secondMoveId = secondMoveId.replace(/\s*\(.*?\)\s*/g, '').trim();
     }
   
     // Procesar partner_id para obtener el texto antes de la coma
-    if (record.partner_id && Array.isArray(record.partner_id) && record.partner_id.length > 1) {
+    if (Array.isArray(record.partner_id) && record.partner_id.length > 1) {
       clientName = record.partner_id[1].trim();
       // Quitar todo lo que está después de la coma, incluida la coma
       clientName = clientName.split(',')[0].trim();
     }
   
     // Procesar product_id
-    if (record.product_id && Array.isArray(record.product_id) && record.product_id.length > 1) {
+    if (Array.isArray(record.product_id) && record.product_id.length > 1) {
       const productString = record.product_id[1];
       const match = productString.match(/^\[(\d+)\]\s(.+)$/);
       if (match) {
@@ -120,49 +120,63 @@ const processedRecords = records.map(record => {
       factorajeAmount: record.factorajeAmount,
       prontoPagoAmount: record.prontoPagoAmount,
       price_subtotal: record.price_subtotal,
+      VentaNeta: record.price_subtotal - record.feeForServiceAmount - record.recuperacionCostoAmount - record.feeLogisticoAmount - record.publicidadAmount - record.factorajeAmount - record.prontoPagoAmount,
       fecha: record.invoice_date
     };
   });
-  // Endpoint para descargar los datos en formato Excel
-app.get('/descargar-excel', (req, res) => {
+  
+  app.get('/descargar', async (req, res) => {
     // Crear un nuevo libro de trabajo y una hoja
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('Datos');
+    let workbook = new ExcelJS.Workbook();
+    let worksheet = workbook.addWorksheet('Datos');
+    // Leer los filtros de la solicitud
+    const { cliente, fechaInicio, fechaFin, busqueda } = req.query;
+
+    // Filtrar `processedRecords` basado en los criterios. Ajusta esta lógica basada en cómo necesitas filtrar los datos.
+    let recordsFiltrados = processedRecords.filter(record => {
+      let cumpleCliente = cliente ? record.partner_id === cliente : true;
+      let cumpleFechaInicio = fechaInicio ? new Date(record.fecha) >= new Date(fechaInicio) : true;
+      let cumpleFechaFin = fechaFin ? new Date(record.fecha) <= new Date(fechaFin) : true;
+      let cumpleBusqueda = busqueda ? record.name.includes(busqueda) : true;
+      return cumpleCliente && cumpleFechaInicio && cumpleFechaFin && cumpleBusqueda;
+    });
+    // Agregar encabezados de columna
+    worksheet.columns = [
+      { header: 'Factura', key: 'move_id', width: 10 },
+      { header: 'Cliente', key: 'partner_id', width: 30 },
+      { header: 'SKU', key: 'SKU', width: 30 },
+      { header: 'Nombre', key: 'name', width: 30 },
+      { header: 'Venta Bruta', key: 'price_subtotal', width: 15 },
+      { header: 'Fee For Service', key: 'feeForServiceAmount', width: 15 },
+      { header: 'Recuperacion de Costo', key: 'recuperacionCostoAmount', width: 15 },
+      { header: 'Fee Logistico', key: 'feeLogisticoAmount', width: 15 },
+      { header: 'Publicidad', key: 'publicidadAmount', width: 15 },
+      { header: 'Factoraje', key: 'factorajeAmount', width: 15 },
+      { header: 'Pronto pago', key: 'prontoPagoAmount', width: 15 },
+      { header: 'Venta neta', key: 'VentaNeta', width: 15 },
+      // Añade más columnas según tus datos
+    ];
   
-    // Agregar fila de encabezados en la hoja de Excel
-    worksheet.addRow([
-      'Factura', 'Cliente', 'SKU', 'Nombre', 'Venta Bruta', 
-      'Fee for Service', 'Recuperación del Costo', 'Fee Logístico', 
-      'Publicidad', 'Factoraje', 'Pronto Pago', 'Venta Neta'
-    ]);
-  
-    // Agregar datos a la hoja de Excel
-    records.forEach(record => {
-      worksheet.addRow([
-        record.move_id, record.partner_id, record.SKU, record.name, 
-        record.price_subtotal, record.feeForServiceAmount, record.recuperacionCostoAmount, 
-        record.feeLogisticoAmount, record.publicidadAmount, record.factorajeAmount, 
-        record.prontoPagoAmount, record.price_subtotal - record.feeForServiceAmount - record.recuperacionCostoAmount - record.feeLogisticoAmount - record.publicidadAmount - record.factorajeAmount - record.prontoPagoAmount
-      ]);
+    // Agregar los datos a la hoja
+    recordsFiltrados.forEach(record => {
+      worksheet.addRow(record);
     });
   
-    // Ajustar las columnas
+    // Ajustar las celdas a los datos
     worksheet.columns.forEach(column => {
-      column.width = column.header.length < 12 ? 12 : column.header.length;
+      column.width = column.width < 20 ? 20 : column.width;
     });
   
-    // Establecer estilos y formato según sea necesario...
-  
-    // Escribir el archivo Excel en el Response Object
+    // Configurar la respuesta para descargar el archivo
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader('Content-Disposition', 'attachment; filename=' + 'reporte.xlsx');
+    res.setHeader('Content-Disposition', 'attachment; filename="reporte.xlsx"');
   
-    workbook.xlsx.write(res)
-      .then(() => {
-        res.end();
-      });
+    // Escribir el libro de trabajo en la respuesta
+    await workbook.xlsx.write(res);
+  
+    // Finalizar la respuesta
+    res.end();
   });
-  
 
     // Inicia el servidor y muestra los registros en la ruta raíz
     app.get('/', (req, res) => {
